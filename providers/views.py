0a6686c9641c45799a0769a03bb3b3596cd716db
Matthew McGowan <matthew.joseph.mcgowan@gmail.com>
2020-08-22 00:31:57 +1200

    Require login for graph signin

--- providers/views.py
+++ providers/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
-from providers.graph.auth import get_sign_in_url, get_token_from_code, get_token, store_token
+from providers.graph.auth import get_sign_in_url, get_token_from_code, get_token, store_token, remove_token
 from providers.common import queries
 from providers.common import commands
 from providers.common import serializers
@@ -11,21 +12,23 @@ from providers.common.utils import get_body_json
 from providers.models import Track
 
 
+@login_required
 def graph_sign_in(request):
     sign_in_url, state = get_sign_in_url()
     request.session['auth_state'] = state
     return HttpResponseRedirect(sign_in_url)
 
 
+@login_required
+def graph_sign_out(request):
+    remove_token(request.user)
+    return HttpResponseRedirect(reverse('home'))
+
+
 def graph_callback(request):
     expected_state = request.session.pop('auth_state', '')
     token = get_token_from_code(request.get_full_path(), expected_state)
     store_token(request.user, token)
-    return HttpResponseRedirect(reverse('home'))
-
-
-def graph_sign_out(request):
-    remove_token(request.user)
     return HttpResponseRedirect(reverse('home'))
 
 
